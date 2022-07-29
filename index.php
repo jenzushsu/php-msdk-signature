a298f764b4f590fb2817759fa2b1ac5138e65676
Jenzus Hsu <jenzushsu@gmail.com>
2022-07-29 14:40:07 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    use \Firebase\JWT\JWT;
+
+    function generateSignature($sdkKey, $sdkSecret, $meetingNumber, $role) {
+        $iat = time();
+        $exp = $iat + 60 * 60 * 2;
+        $token_payload = [
+            'sdkKey' => $sdkKey,
+            'iat' => $iat,
+            'exp' => $exp,
+            'mn' => $meetingNumber,
+            'role' => $role
+        ];
+        $jwt = JWT::encode($token_payload, $sdkSecret, 'HS256');
+        
+        return $jwt;
+    }
+    // echo($_ENV['SDK_KEY']);
+    echo(generateSignature($_ENV['SDK_KEY'], $_ENV['SDK_SECRET'], 12345678912, 0));
